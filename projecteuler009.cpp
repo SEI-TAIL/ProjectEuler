f56dd110dba650ba7f45e8633cf2c38c369d16c7
REINV-WHEEL <file_tree@yahoo.co.jp>
2010-08-22 00:48:13 +0900

    ProjectEuler 9 - 10

---
+++ projecteuler009.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int projecteuler009(void) {
+	int a, b, c;
+	
+	for (a = 1; a < 1000; a++) {
+		for (b = a; a+b < 1000; b++) {
+			c = 1000 - a - b;
+			if (a*a + b*b == c*c) {
+				printf("a %d, b %d, c %d\n", a, b, c);
+				printf("%d + %d = %d = %d\n", a*a, b*b, a*a+b*b, c*c);
+				return a * b * c;
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ projecteuler010.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+long long projecteuler010(void) {
+	int i, j;
+	long long sum = 2LL;
+	for (i = 3; i <= 2000000; i += 2) {
+		bool isPrime = true;
+		for (j = 3; j*j <= i; j += 2) {
+			if (i % j == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime == true) {
+			sum += (long long)i;
+		}
+	}
+	printf("%lld\n", sum);
+	return sum;
+}
