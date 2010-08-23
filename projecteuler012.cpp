a6817e7060d9b20c1e98681fd2f4f4a7832ef6cc
REINV-WHEEL <file_tree@yahoo.co.jp>
2010-08-23 23:14:26 +0900

    ProjectEuler 12 - 13 and Ruby 13 and Perl 3

---
+++ projecteuler012.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int projecteuler012(void) {
+	int triangle = 0;
+	int factorNum;
+	int i, j, k = 0;
+	for (i = 1;; i++) {
+		triangle += i;
+		factorNum = 0;
+		for (j = 1; j*j < triangle; j++) {
+			if (triangle % j == 0) factorNum++;
+		}
+		factorNum *= 2;
+		if (j*j <= triangle) factorNum++;
+		if (factorNum >= 500) return triangle;
+		printf("%d %d %d\n", i, triangle, factorNum);
+	}
+	return 0;
+}
---
+++ projecteuler013.cpp
@@ -0,0 +1,69 @@
+﻿#include <stdio.h>
+
+int projecteuler013(void) {
+	long long ValueLine[100][3];
+	long long bufVal;
+	const int MAX_DIGIT = 18;
+	int buf, i, j, k;
+	FILE *fp;
+
+	if ((fp = fopen("projecteuler013.txt", "r")) == NULL) {
+		return -1;
+	}
+
+	for (i = 0; i < 100; i++) {
+		for (j = 0; j < 3; j++) {
+			ValueLine[i][j] = 0;
+		}
+	}
+
+	for (i = 0, j = 0, k = 0; (buf = getc(fp)) != EOF;) {
+		if (buf == '\n') {
+			printf(" 改行 \n");
+			i++;
+			j = 0;
+			k = 0;
+			continue;
+		}
+
+		if (k < MAX_DIGIT) {
+			k++;
+		} else {
+			j++;
+			if (j >= 3) {
+				j = 0;
+				i++;
+			}
+			k = 1;
+		}
+
+		ValueLine[i][j] *= 10;
+		ValueLine[i][j] += (buf - '0');
+		printf("%d %d %d\n", i, j, k);
+
+		if (j == 0 && k == 14) {
+			j++;
+			k = 0;
+		}
+	}
+
+	for (i = 0; i < 100; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%lld ", ValueLine[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (j = 0; j < 3; j++) {
+		bufVal = 0;
+		for (i = 0; i < 100; i++) {
+			bufVal += ValueLine[i][j];
+			if (bufVal / 1000000000000000000LL >= 1LL) {
+				ValueLine[0][j]++;
+				bufVal -= 1000000000000000000LL;
+			}
+		}
+		printf("\n\n%lld ", bufVal);
+	}
+	return 0;
+}
