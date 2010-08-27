86ae15b1202727d0373c5995400487663c369cc3
REINV-WHEEL <file_tree@yahoo.co.jp>
2010-08-28 04:27:38 +0900

    Problem 18 - 19

---
+++ projecteuler018_02.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <fstream>
+#define max(x,y) (x>y?x:y)
+
+int topBottomTriangle(int i, int j);
+
+int fileTable[15][15];
+
+int projecteuler018(void) {
+	int i, j;
+	int n = 15;
+	int triMax;
+
+	std::ifstream cin("projecteuler18.txt");
+
+	for(i = 0; i < n; i++) {
+		for(int j=0;j<=i;j++) {
+			cin >> fileTable[i][j];
+		}
+	}
+
+	triMax = topBottomTriangle(0, 0);
+	printf("%d\n", triMax);
+	for (i = 0; i < 15; i++) {
+		for (j = 0; j < i; j++) {
+			printf("%d ", fileTable[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+int topBottomTriangle(int i, int j) {
+	int val;
+	int right, left;
+	int nextMax;
+	val = fileTable[i][j];
+	if (i == 14) return val;
+	left = topBottomTriangle(i+1, j);
+	right = topBottomTriangle(i+1, j+1);
+	return val + max(left, right);
+}
---
+++ projecteuler019_02.cpp
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+
+int projecteuler019(void) {
+	int weekday = 1, day, month, year;
+	int sundayNum = 0;
+	year = 1900;
+	month = 1;
+	day = 1;
+	weekday = 1;
+	for (;year <= 2000;) {
+		switch (month) {
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				weekday = (weekday + 31)%7;
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				weekday = (weekday + 30)%7;
+				break;
+			case 2:
+				if (year % 4 != 0 || (year % 400 != 0 && year % 100 == 0)) weekday = (weekday + 28)%7;
+				else weekday = (weekday + 29)%7;
+				break;
+		}
+
+		if (year > 1900 && weekday == 0) {
+			sundayNum++;
+		}
+
+		if (month < 12) month++;
+		else {
+			month = 1;
+			year++;
+		}
+
+		printf("%d %d %d\n", year, month, weekday);
+
+	}
+
+	printf("%d\n", sundayNum);
+
+	return sundayNum;
+}
