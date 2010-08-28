b8f4e348754c5863ef783950502510eacfd7c945
REINV-WHEEL <file_tree@yahoo.co.jp>
2010-08-29 04:23:31 +0900

    Problem 20 - 21

---
+++ projecteuler020.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int projecteuler020(void) {
+	int i, j, n = 100;
+	int sum = 0;
+	int digit[1000];
+
+	for (i = 0; i < 1000; i++) digit[i] = 0;
+	
+	digit[0] = 1;
+	for (i = n; i > 0; i--) {
+		for (j = 0; j < 1000; j++) {
+			digit[j] *= i;
+		}
+		for (j = 0; j < 1000; j++) {
+			if (digit[j] >= 10) {
+				digit[j+1] += (digit[j] / 10);
+				digit[j] = digit[j] % 10;
+			}
+		}
+	}
+
+	for (i = 0; i < 200; i++) {
+		sum += digit[i];
+	}
+
+	printf("\n%d\n", sum);
+
+	return sum;
+}
---
+++ projecteuler021.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <map>
+
+int projecteuler021(void) {
+	int i, j;
+	int sumFactor;
+	int sumAmicable = 0;
+	std::map<int, int> memo;
+
+	for (i = 1; i < 10000; i++) {
+		int amicable = 0;
+		int sumFactor = 0;
+		if (memo[i] == 1) {
+			continue;
+		}
+		for (j = 1; j <= i/2; j++) {
+			if (i % j == 0) {
+				sumFactor += j;
+			}
+		}
+		for (j = 1; j <= sumFactor/2; j++) {
+			if (sumFactor % j == 0) {
+				amicable += j;
+			}
+		}
+		if (amicable == i && amicable != sumFactor) {
+			sumAmicable += sumFactor;
+			sumAmicable += amicable;
+			memo[sumFactor] = 1;
+			memo[amicable] = 1;
+			printf("%d %d\n", sumFactor, amicable);
+		} else {
+			memo[sumFactor] = 1;
+		}
+	}
+
+	printf("%d\n", sumAmicable);
+
+	return sumAmicable;
+}
