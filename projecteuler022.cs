14c0897fa98bd8e966a3cb6194b2cc0cc96fc3a9
REINV-WHEEL <file_tree@yahoo.co.jp>
2010-09-03 12:23:33 +0900

    Source codes audiences give me.

---
+++ projecteuler022.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+ 
+namespace ProjectEuler
+{
+    class NO_0022
+    {
+        public static void Main()
+        {
+            long sum = 0;
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(@"names.txt"))
+            {
+                var namelist = sr.ReadToEnd().Split(',').Select(name => name.Replace("\"", ""))
+                    .OrderBy(name => name).Select((name,index) => new {index, name});
+                foreach (var person in namelist)
+                {
+                    //Console.WriteLine("{0} {1}", person.index, person.name);
+                    var score = (person.index + 1)
+                        * person.name.ToCharArray().Aggregate(0, (nsum, chara) => nsum + ((int)chara - 'A' + 1));
+                    sum += score;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ projecteuler022_01.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+int main(){
+	ifstream cin("names.txt");
+	vector<string> names;
+	
+	string in, tmp, name;
+	cin >> in;
+	
+	for(int i=0;i<in.size();i++){
+		if(in[i]=='"')continue;
+		if(in[i]==',')tmp += ' '; else tmp += in[i];
+	}
+	
+	stringstream ss(tmp);
+	while(ss >> name)names.push_back(name);
+	
+	sort(names.begin(),names.end());
+	
+	int sum = 0;
+	for(int i=0;i<names.size();i++){
+		int score = 0;
+		for(int j=0;j<names[i].size();j++)score += names[i][j]-'A' + 1;
+		sum += score * (i + 1);
+	}
+	
+	cout << sum << endl;
+	return 0;
+}
